60562363858752508c67b1cf34a7630719819489
Lior Savchenko <62830736+liorsavc@users.noreply.github.com>
2020-10-12 15:07:38 +0300

    Updates

---
+++ precache-manifest.c54b223c16f38872a7f0a69968a9609c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd62ab19f0f6cdb02ea032dc79631df",
+    "url": "/my_bio/index.html"
+  },
+  {
+    "revision": "72c7943cce76d3a2eeab",
+    "url": "/my_bio/static/css/main.cca91c5e.chunk.css"
+  },
+  {
+    "revision": "b82898366554da1204c6",
+    "url": "/my_bio/static/js/2.7be81624.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_bio/static/js/2.7be81624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c7943cce76d3a2eeab",
+    "url": "/my_bio/static/js/main.073539f9.chunk.js"
+  },
+  {
+    "revision": "99f3f568cfe85afbdd36",
+    "url": "/my_bio/static/js/runtime-main.f3890902.js"
+  },
+  {
+    "revision": "c2d5ab35b9c859d6b02a91c340c81123",
+    "url": "/my_bio/static/media/education.c2d5ab35.jpg"
+  },
+  {
+    "revision": "cacdde3cc6d4fd22e5f7b36cb7796f52",
+    "url": "/my_bio/static/media/experience.cacdde3c.jpg"
+  },
+  {
+    "revision": "2f7bb66ac1cfc038a0520932e2f5ef7b",
+    "url": "/my_bio/static/media/profilePic.2f7bb66a.jpg"
+  },
+  {
+    "revision": "ad08e6ef67536679695af945753e19c2",
+    "url": "/my_bio/static/media/programingSkills.ad08e6ef.jpg"
+  },
+  {
+    "revision": "f60ea5ecedfa995da8713980a38026de",
+    "url": "/my_bio/static/media/projects.f60ea5ec.jpg"
+  }
+]);
